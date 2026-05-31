a41af24aa6ce04adbeec5b061f500544140fb7dd
ShraddhaShandilya119 <shraddhashandilya2005@gmail.com>
2026-05-31 18:13:34 +0530

    fix uploads folder creation

--- backend_ai/utils/multer.js
+++ backend_ai/utils/multer.js
@@ -1,10 +1,15 @@
 let multer = require('multer');
 const path = require("path");
+const fs = require("fs");
 
-// Multer config
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads", { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + path.extname(file.originalname))
 });
 
 const fileFilter = (req, file, cb) => {
