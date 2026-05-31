4b75ba5460d39cb2d425d0c1a2d1221851a7ac23
ShraddhaShandilya119 <shraddhashandilya2005@gmail.com>
2026-05-31 13:23:44 +0530

    fix CORS issue for production deployment

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -9,13 +9,27 @@ const PORT = process.env.PORT || 4000;
 require('./conn');
 const allowedOrigins = [
     "http://localhost:5173",
+    "http://localhost:5174",
+    "https://ai-resume-screening-7zmld0fcy-shraddhashandilya119s-projects.vercel.app",
     "https://ai-resume-screening-khaki.vercel.app"
 ];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        // allow Postman / server-to-server
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(null, false);
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
+
+app.options("*", cors());
 app.use(express.json());
 
 
