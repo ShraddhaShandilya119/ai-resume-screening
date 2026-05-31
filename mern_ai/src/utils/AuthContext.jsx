adfcbb4a9cf27564b335be161085d09b480d0d56
ShraddhaShandilya119 <shraddhashandilya2005@gmail.com>
2026-05-31 11:20:43 +0530

    fix auth context persistence bug

--- mern_ai/src/utils/AuthContext.jsx
+++ mern_ai/src/utils/AuthContext.jsx
@@ -1,21 +1,32 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({children})=>{
+const AuthProvider = ({ children }) => {
 
-    var login = localStorage.getItem('isLogin');
-    var userInfoData = localStorage.getItem("userInfo");
-    const [isLogin,setLogin] = useState(login?login:false);
-    const [userInfo,setUserInfo] = useState(userInfoData?JSON.parse(userInfoData):null);
+    const login = localStorage.getItem('isLogin');
+    const userInfoData = localStorage.getItem("userInfo");
 
-    return(
-        <AuthContext.Provider value={{isLogin,setLogin,userInfo,setUserInfo}}>
+    const [isLogin, setLogin] = useState(login === "true");
+    const [userInfo, setUserInfo] = useState(
+        userInfoData ? JSON.parse(userInfoData) : null
+    );
+
+    useEffect(() => {
+        const login = localStorage.getItem("isLogin");
+        const user = localStorage.getItem("userInfo");
+
+        if (login === "true" && user) {
+            setLogin(true);
+            setUserInfo(JSON.parse(user));
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ isLogin, setLogin, userInfo, setUserInfo }}>
             {children}
         </AuthContext.Provider>
     );
-
-}
+};
 
 export default AuthProvider;
--- mern_ai/src/utils/axios.js
+++ mern_ai/src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://ai-resume-screening-0p6n.onrender.com",
+    withCredentials: true
    
 });
 
